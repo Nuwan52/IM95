0831e512494b0f25b7a50f1e62615bc533090972
Nuwan <snuwan098@gmail.com>
2025-09-02 04:59:14 +0530

    commit

--- rs485.py
+++ rs485.py
@@ -6,7 +6,6 @@ def rs485_task(socketio):
     Replace the loop with your real RS485 read/write logic.
     """
     while True:
-        # TODO: replace this with actual RS485 read/write code
         print("RS485 thread is running...")
 
         # Example: send a message to UI
